808d94923a94d61096d316be31a6b1d8b44691f0
Saqib-17 <sakib17shahidul@gmail.com>
2025-04-22 13:47:54 +0600

    Fixed API issues

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://donation-client-six.vercel.app'],
+  methods: ['GET', 'POST'],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 // Routes
